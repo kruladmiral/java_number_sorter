da3fc88009bb391198e953b41dbf7d10a59d16dc
Wojciech Sęk <wojtasskate@wp.pl>
2024-01-29 21:34:58 +0100

    testing

--- sorter/numbersorter.java
+++ sorter/numbersorter.java
@@ -1,8 +1,8 @@
-package src;
+package sorter;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Tablica {
+public class numbersorter {
 	public static Scanner scanner = new Scanner(System.in);
 	public static void main(String[] args) {
 		System.out.println("Podaj liczbę");
